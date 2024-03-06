a2ef4cf99c1ea63eb9ff59771a14d85ea8e3d647
riccardovacirca <rvacirca23@gmail.com>
2024-03-06 09:40:18 +0000

    updated

--- ns-microservices/ns-users/nsusers.ts
+++ ns-microservices/ns-users/nsusers.ts
@@ -1,15 +1,24 @@
+
+import * as fs from 'fs';
+
 export const UserEntity = (id:number) => ({
   id, components:[] as Object[]
 });
 
 export const UserDataComponent = (
-  id:number, username:string, created_at:string, updated_at:string, deleted_at:string) => ({
-  id, username, created_at, updated_at, deleted_at
+  id:number, username:string, createdAt:string, updatedAt:string,
+  deletedAt:string) => ({
+  id, username, createdAt, updatedAt, deletedAt
 });
 
 export const UsersListComponent = (entries:any[]) => ({entries});
 
-export const fetchUsersListSystem = async () => {
+export const nsReadConfig = (path) => {
+  const conf = fs.readFileSync(path, 'utf-8');
+  return JSON.parse(conf);
+};
+
+export const nsFetchUsersList = async () => {
   try {
     const resp = await fetch(`http://192.168.1.5:2310/api/users-list`);
     if (!resp.ok) {throw new Error();}
@@ -21,7 +30,7 @@ export const fetchUsersListSystem = async () => {
   }
 };
 
-export const createUserDataSystem = async (uData:any) => {
+export const nsCreateUserData = async (uData:any) => {
   try {
     const resp = await fetch('/api/user', {
       method: 'POST', headers: {'Content-Type': 'application/json'},
@@ -34,7 +43,7 @@ export const createUserDataSystem = async (uData:any) => {
   }
 };
 
-export const readUserDataSystem = async (uid:number) => {
+export const nsReadUserData = async (uid:number) => {
   try {
     const resp = await fetch(`/api/users/${uid}`);
     if (!resp.ok) {throw new Error();}
@@ -46,7 +55,7 @@ export const readUserDataSystem = async (uid:number) => {
   }
 };
 
-export const updateUserDataSystem = async (uData:any) => {
+export const nsUpdateUserData = async (uData:any) => {
   try {
     const resp = await fetch(`/api/user/${uData.id}`, {
       method: 'PUT', headers: {'Content-Type': 'application/json'},
@@ -59,7 +68,7 @@ export const updateUserDataSystem = async (uData:any) => {
   }
 };
 
-export const deleteUserDataSystem = async (user:any) => {
+export const nsDeleteUserData = async (user:any) => {
   try {
     const resp = await fetch(`/api/users/${user.id}`, {method: 'DELETE'});
     if (!resp.ok) {throw new Error();}
